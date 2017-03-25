de310a53fd66d6335dbbe10f15b9db93b3114815
Joshua Barta <Joshua_Barta@DellTeam.com>
2017-03-25 00:39:28 -0500

    initial product editor stuff

--- jzb-store/src/app/app.component.js
+++ jzb-store/src/app/app.component.js
@@ -6,16 +6,32 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 var core_1 = require("@angular/core");
+var Product = (function () {
+    function Product() {
+    }
+    return Product;
+}());
+exports.Product = Product;
 var AppComponent = (function () {
     function AppComponent() {
-        this.name = 'Angular';
+        this.title = 'Josh\'s Super Awesome Store';
+        this.product = {
+            id: 1,
+            sku: 'LS-BLU',
+            name: 'Jedi Lightsaber',
+            variant: 'Blue',
+            description: 'For over a thousand generations, the Jedi Knights were the guardians of peace and justice in the Old Republic. Now you too can protect the galaxy with this real working lightsaber!',
+            price: '700000',
+            currency: 'imperial credits',
+            disclaimer: 'Not responsible for death, dismemberment, or loss of limb caused by misuse of product. Seriously, this is a blade of pure energy intended for use by highly-trained space wizards. Do you really need a warning?'
+        };
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "<h1>Hello {{name}}</h1>",
+        template: "\n  <h1>{{title}}</h1>\n  <div class=\"box\">\n\t<h2>{{product.name}} ({{product.variant}})</h2>\n\t<div>&nbsp;\n\t<div class=\"sku\">SKU#{{product.sku}}</div>\n\t\n\t<div class=\"price\">{{product.price}} {{product.currency}}</div>\n\t</div>\n\t<div class=\"details\">{{product.description}}</div>\n\t<div class=\"tiny\">{{product.disclaimer}}</div>\n   </div>\n   \n   <div class=\"box\">\n    <div>\n\t\t<label>id: </label>{{product.id}}\n\t\t\n\t\t<label>sku:</label>\n\t\t<input [(ngModel)]=\"product.sku\" placeholder=\"sku\" />\n\t</div>\n\t<div>\n\t\t\n\t\t\n\t\t<label>price:</label>\n\t\t<input [(ngModel)]=\"product.price\" placeholder=\"price\" />\n\t\t\n\t\t<label>currency:</label>\n\t\t<input [(ngModel)]=\"product.currency\" placeholder=\"currency\" />\n\t</div>\n\t<div>\n\t\t<label>name:</label>\n\t\t<input [(ngModel)]=\"product.name\" placeholder=\"name\" />\n\t\t\n\t\t<label>variant:</label>\n\t\t<input [(ngModel)]=\"product.variant\" placeholder=\"name\" />\n\t</div>\n\t<div>\n\t\t<label>description</label><br/>\n\t\t<textarea [(ngModel)]=\"product.description\" placeholder=\"description\" ></textarea>\n\t</div>\n\t<div>\n\t\t<label>disclaimer</label><br/>\n\t\t<textarea [(ngModel)]=\"product.disclaimer\" placeholder=\"disclaimer\" ></textarea>\n\t</div>\n    </div>\n   \n\t\n   \n  ",
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
--- jzb-store/src/app/app.component.ts
+++ jzb-store/src/app/app.component.ts
@@ -1,7 +1,78 @@
 import { Component } from '@angular/core';
 
+export class Product {
+	id: number;
+	sku: string;
+	name: string;
+	variant: string;
+	description: string;
+	price: number;
+	currency: string;
+	disclaimer: string;
+}
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+  <h1>{{title}}</h1>
+  <div class="box">
+	<h2>{{product.name}} ({{product.variant}})</h2>
+	<div>&nbsp;
+	<div class="sku">SKU#{{product.sku}}</div>
+	
+	<div class="price">{{product.price}} {{product.currency}}</div>
+	</div>
+	<div class="details">{{product.description}}</div>
+	<div class="tiny">{{product.disclaimer}}</div>
+   </div>
+   
+   <div class="box">
+    <div>
+		<label>id: </label>{{product.id}}
+		
+		<label>sku:</label>
+		<input [(ngModel)]="product.sku" placeholder="sku" />
+	</div>
+	<div>
+		
+		
+		<label>price:</label>
+		<input [(ngModel)]="product.price" placeholder="price" />
+		
+		<label>currency:</label>
+		<input [(ngModel)]="product.currency" placeholder="currency" />
+	</div>
+	<div>
+		<label>name:</label>
+		<input [(ngModel)]="product.name" placeholder="name" />
+		
+		<label>variant:</label>
+		<input [(ngModel)]="product.variant" placeholder="name" />
+	</div>
+	<div>
+		<label>description</label><br/>
+		<textarea [(ngModel)]="product.description" placeholder="description" ></textarea>
+	</div>
+	<div>
+		<label>disclaimer</label><br/>
+		<textarea [(ngModel)]="product.disclaimer" placeholder="disclaimer" ></textarea>
+	</div>
+    </div>
+   
+	
+   
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+	title = 'Josh\'s Super Awesome Store',
+	product = {
+		id: 1,
+		sku: 'LS-BLU',
+		name: 'Jedi Lightsaber',
+		variant: 'Blue',
+		description: 'For over a thousand generations, the Jedi Knights were the guardians of peace and justice in the Old Republic. Now you too can protect the galaxy with this real working lightsaber!',
+		price: '700000',
+		currency: 'imperial credits',
+		disclaimer: 'Not responsible for death, dismemberment, or loss of limb caused by misuse of product. Seriously, this is a blade of pure energy intended for use by highly-trained space wizards. Do you really need a warning?'
+	};
+}
