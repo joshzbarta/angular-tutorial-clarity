8fcf9cded09516e8b87ef89d04377a274a8f0e55
Joshua Barta <Joshua_Barta@DellTeam.com>
2017-03-26 02:54:36 -0500

    start setting up list of products, stylize list, constructor to Product class

--- jzb-store/src/app/app.component.js
+++ jzb-store/src/app/app.component.js
@@ -8,30 +8,46 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var core_1 = require("@angular/core");
 var Product = (function () {
     function Product() {
+        this.variant = '';
+        this.price = 0;
+        this.currency = '';
     }
     return Product;
 }());
 exports.Product = Product;
+var PRODUCTS = [
+    {
+        id: 1,
+        sku: 'LS-BLU',
+        name: 'Jedi Lightsaber',
+        variant: 'Blue',
+        description: 'For over a thousand generations, the Jedi Knights were the guardians of peace and justice in the Old Republic. Now you too can protect the galaxy with this real working lightsaber!',
+        price: 700000,
+        currency: 'imperial credits',
+        availability: '',
+        disclaimer: 'Not responsible for death, dismemberment, or loss of limb caused by misuse of product. Seriously, this is a blade of pure energy intended for use by highly-trained space wizards. Do you really need a warning?'
+    },
+    {
+        id: 2,
+        sku: 'FLUX-1985',
+        name: 'Flux Capacitor',
+        description: 'The great Dr. Emmett \"Doc\" Brown came up with the idea for the flux capacitor after falling off the toilet he was standing on while hanging a clock and hitting his head on the sink. Skip the concussion and order your own flux capacitor today ',
+        availability: 'Sold Out',
+        disclaimer: 'Not responsible for universe-destroying paradoxes.	Activation requires 1.21 gigawatts of electricity. Fusion power recommended, will run on plutonium or lightning. Compatibility not guaranteed with vehicles other than 1981, 1982, or 1983 DeLorean'
+    }
+];
 var AppComponent = (function () {
     function AppComponent() {
-        this.title = 'Josh\'s Super Awesome Store3';
-        this.product = {
-            id: 1,
-            sku: 'LS-BLU',
-            name: 'Jedi Lightsaber',
-            variant: 'Blue',
-            description: 'For over a thousand generations, the Jedi Knights were the guardians of peace and justice in the Old Republic. Now you too can protect the galaxy with this real working lightsaber!',
-            price: '700000',
-            currency: 'imperial credits',
-            disclaimer: 'Not responsible for death, dismemberment, or loss of limb caused by misuse of product. Seriously, this is a blade of pure energy intended for use by highly-trained space wizards. Do you really need a warning?'
-        };
+        this.title = 'Josh\'s Super Awesome Store 3';
+        this.products = PRODUCTS;
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "\n  <h1>{{title}}</h1>\n  <div class=\"box\">\n\t<h2>{{product.name}} ({{product.variant}})</h2>\n\t<div>&nbsp;\n\t<div class=\"sku\">SKU#{{product.sku}}</div>\n\t\n\t<div class=\"price\">{{product.price}} {{product.currency}}</div>\n\t</div>\n\t<div class=\"details\">{{product.description}}</div>\n\t<div class=\"tiny\">{{product.disclaimer}}</div>\n   </div>\n   \n   <div class=\"box\">\n    <div>\n\t\t<label>id: </label>{{product.id}}\n\t\t\n\t\t<label>sku:</label>\n\t\t<input [(ngModel)]=\"product.sku\" placeholder=\"sku\" />\n\t</div>\n\t<div>\n\t\t\n\t\t\n\t\t<label>price:</label>\n\t\t<input [(ngModel)]=\"product.price\" placeholder=\"price\" />\n\t\t\n\t\t<label>currency:</label>\n\t\t<input [(ngModel)]=\"product.currency\" placeholder=\"currency\" />\n\t</div>\n\t<div>\n\t\t<label>name:</label>\n\t\t<input [(ngModel)]=\"product.name\" placeholder=\"name\" />\n\t\t\n\t\t<label>variant:</label>\n\t\t<input [(ngModel)]=\"product.variant\" placeholder=\"name\" />\n\t</div>\n\t<div>\n\t\t<label>description</label><br/>\n\t\t<textarea [(ngModel)]=\"product.description\" placeholder=\"description\" ></textarea>\n\t</div>\n\t<div>\n\t\t<label>disclaimer</label><br/>\n\t\t<textarea [(ngModel)]=\"product.disclaimer\" placeholder=\"disclaimer\" ></textarea>\n\t</div>\n    </div>\n   \n\t\n   \n  ",
+        template: "\n  \n  <h1>{{title}}</h1>\n  <ul class=\"product-list\">\n\t<li *ngFor=\"let product of products\">\n\t\t<span class=\"product-id\">\n\t\t\t{{product.id}}\n\t\t</span>\n\t\t<span class=\"product-sku\">{{product.sku}}</span>\n\t\t\n\t\t<span class=\"product-name\" >\n\t\t\t<span>{{product.name}}</span> \n\t\t \n\t\t\t<span *ngIf=\"product.variant\">({{product.variant}})</span>\n\t\t </span>\n\t</li>\n  </ul>\n\n  <!--\n    \n  \n  <div class=\"box\">\n\t<h2>{{product.name}} ({{product.variant}})</h2>\n\t<div>&nbsp;\n\t<div class=\"sku\">SKU#{{product.sku}}</div>\n\t\n\t<div class=\"price\">{{product.price}} {{product.currency}}</div>\n\t</div>\n\t<div class=\"details\">{{product.description}}</div>\n\t<div class=\"tiny\">{{product.disclaimer}}</div>\n   </div>\n   \n   <div class=\"box\">\n    <div>\n\t\t<label>id: </label>{{product.id}}\n\t\t\n\t\t<label>sku:</label>\n\t\t<input [(ngModel)]=\"product.sku\" placeholder=\"sku\" />\n\t</div>\n\t<div>\n\t\t\n\t\t\n\t\t<label>price:</label>\n\t\t<input [(ngModel)]=\"product.price\" placeholder=\"price\" />\n\t\t\n\t\t<label>currency:</label>\n\t\t<input [(ngModel)]=\"product.currency\" placeholder=\"currency\" />\n\t\t\n\t\t<label>availability:</label>\n\t\t<input [(ngModel)]=\"product.availability\" \n\t\t\t   placeholder=\"availability\" />\n\t</div>\n\t<div>\n\t\t<label>name:</label>\n\t\t<input [(ngModel)]=\"product.name\" placeholder=\"name\" />\n\t\t\n\t\t<label>variant:</label>\n\t\t<input [(ngModel)]=\"product.variant\" placeholder=\"name\" />\n\t</div>\n\t<div>\n\t\t<label>description</label><br/>\n\t\t<textarea [(ngModel)]=\"product.description\" placeholder=\"description\" ></textarea>\n\t</div>\n\t<div>\n\t\t<label>disclaimer</label><br/>\n\t\t<textarea [(ngModel)]=\"product.disclaimer\" placeholder=\"disclaimer\" ></textarea>\n\t</div>\n    </div>-->\n  ",
+        styles: ["\n\t.product-list {\n\t\tmargin: 0 0 2em 0;\n\t\tlist-style-type: none;\n\t\tpadding: 0;\n\t\twidth: 40em;\n\t}\n\t.product-list .product-id {\n\t\twidth: 2em;\n\t\tdisplay: inline-block;\n\t}\n\t.product-list .product-sku {\n\t\twidth: 8em;\n\t\tdisplay: inline-block;\n\t}\n\t.selected {\n\t\tbackground-color: #CFD8DC !important;\n\t\tcolor: white;\n\t}\n\t\n  .product-list li {\n    cursor: pointer;\n    position: relative;\n    left: 0;\n    background-color: #EEE;\n    margin: .5em;\n    padding: .2em .5em .2em .5em;\n    height: 1.6em;\n    border-radius: 4px;\n  }\n  .product-list li.selected:hover {\n    background-color: #BBD8DC !important;\n    color: white;\n  }\n  .product-list li:hover {\n    color: #607D8B;\n    background-color: #DDD;\n    left: .1em;\n\tfont-weight: bold;\n  }\n  .product-list .product-name, .product-list .product-id, .product-list .product-sku  {\n    position: relative;\n    top: 3px;\n  }\n  \n  "]
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
--- jzb-store/src/app/app.component.ts
+++ jzb-store/src/app/app.component.ts
@@ -8,13 +8,60 @@ export class Product {
 	description: string;
 	price: number;
 	currency: string;
+	availability: string;
 	disclaimer: string;
+	constructor(){
+		this.variant = '';
+		this.price=0;
+		this.currency='';
+	}
 }
+
+const PRODUCTS: Product[] = [
+	{
+		id: 1,
+		sku: 'LS-BLU',
+		name: 'Jedi Lightsaber',
+		variant: 'Blue',
+		description: 'For over a thousand generations, the Jedi Knights were the guardians of peace and justice in the Old Republic. Now you too can protect the galaxy with this real working lightsaber!',
+		price: 700000,
+		currency: 'imperial credits',
+		availability: '',
+		disclaimer: 'Not responsible for death, dismemberment, or loss of limb caused by misuse of product. Seriously, this is a blade of pure energy intended for use by highly-trained space wizards. Do you really need a warning?'
+	},
+	{
+		id: 2,
+		sku: 'FLUX-1985',
+		name: 'Flux Capacitor',
+		description: 'The great Dr. Emmett \"Doc\" Brown came up with the idea for the flux capacitor after falling off the toilet he was standing on while hanging a clock and hitting his head on the sink. Skip the concussion and order your own flux capacitor today ',
+		availability: 'Sold Out',
+		disclaimer: 'Not responsible for universe-destroying paradoxes.	Activation requires 1.21 gigawatts of electricity. Fusion power recommended, will run on plutonium or lightning. Compatibility not guaranteed with vehicles other than 1981, 1982, or 1983 DeLorean'
+	}
+];
 
 @Component({
   selector: 'my-app',
   template: `
+  
   <h1>{{title}}</h1>
+  <ul class="product-list">
+	<li *ngFor="let product of products">
+		<span class="product-id">
+			{{product.id}}
+		</span>
+		<span class="product-sku">{{product.sku}}</span>
+		
+		<span class="product-name" >
+			<span>{{product.name}}</span> 
+		 
+			<span *ngIf="product.variant">({{product.variant}})</span>
+		 </span>
+	</li>
+  </ul>
+
+  <!--
+    
+  
   <div class="box">
 	<h2>{{product.name}} ({{product.variant}})</h2>
 	<div>&nbsp;
@@ -41,6 +88,10 @@ export class Product {
 		
 		<label>currency:</label>
 		<input [(ngModel)]="product.currency" placeholder="currency" />
+		
+		<label>availability:</label>
+		<input [(ngModel)]="product.availability" 
+			   placeholder="availability" />
 	</div>
 	<div>
 		<label>name:</label>
@@ -57,22 +108,59 @@ export class Product {
 		<label>disclaimer</label><br/>
 		<textarea [(ngModel)]="product.disclaimer" placeholder="disclaimer" ></textarea>
 	</div>
-    </div>
-   
-	
-   
+    </div>-->
   `,
+  styles: [`
+	.product-list {
+		margin: 0 0 2em 0;
+		list-style-type: none;
+		padding: 0;
+		width: 40em;
+	}
+	.product-list .product-id {
+		width: 2em;
+		display: inline-block;
+	}
+	.product-list .product-sku {
+		width: 8em;
+		display: inline-block;
+	}
+	.selected {
+		background-color: #CFD8DC !important;
+		color: white;
+	}
+	
+  .product-list li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .2em .5em .2em .5em;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .product-list li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+  }
+  .product-list li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+	font-weight: bold;
+  }
+  .product-list .product-name, .product-list .product-id, .product-list .product-sku  {
+    position: relative;
+    top: 3px;
+  }
+  
+  `]
 })
+
+
+
 export class AppComponent  { 
-	title = 'Josh\'s Super Awesome Store3';
-	product = {
-		id: 1,
-		sku: 'LS-BLU',
-		name: 'Jedi Lightsaber',
-		variant: 'Blue',
-		description: 'For over a thousand generations, the Jedi Knights were the guardians of peace and justice in the Old Republic. Now you too can protect the galaxy with this real working lightsaber!',
-		price: '700000',
-		currency: 'imperial credits',
-		disclaimer: 'Not responsible for death, dismemberment, or loss of limb caused by misuse of product. Seriously, this is a blade of pure energy intended for use by highly-trained space wizards. Do you really need a warning?'
-	};
+	title = 'Josh\'s Super Awesome Store 3';
+	products = PRODUCTS;
 }
